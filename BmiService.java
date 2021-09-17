2c5cb770b70e03be7ac9fa86175301b5112fb6a4
komshuk <komshuk@yandex.ru>
2021-09-17 13:38:44 +0300

    Commit1

---
+++ BmiService.java
@@ -0,0 +1,6 @@
+public class BmiService {
+    public double calculate(int weight, double height) {
+        double massIndex = weight / (height * height);
+        return weight / (height * height);
+    }
+}
